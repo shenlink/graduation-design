ec7294eb8815c8a4f010fbcc2ae5898e75e0cee0
shenlink <2024133718@qq.com>
2020-02-04 15:52:33 +0800

    解决了冲突

--- public/test_db.php
+++ public/test_db.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 require_once 'Db.php';
@@ -6,9 +5,4 @@ $sql = new Db();
 // $res = $db->table('article')->where(array('id'=>17,'title'=>'abcd'))->select();
 $res = $db->table('article')->where(array('ad'=>1,'title'=>'php'))->select();
 var_dump($res);
-=======
-<php
-require_once("Db.class.php");
-$sql = new Db();
-var_dump($db);
->>>>>>> cb2f954ba7a4ba5987403034e54668adad333006
+
