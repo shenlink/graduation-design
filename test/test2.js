ec0471c14792b6783ec80c8f8a5e1a76f4ba4aaf
shenlink <2024133718@qq.com>
2020-02-08 21:42:31 +0800

    修复了登录页的缺陷，解决了表单提交验证不通过阻止提交的问题，有一些疑似js语言的BUG，已归类到test文件夹内

---
+++ test/test2.js
@@ -0,0 +1,43 @@
+// function check() {
+//     if (true) {
+//         return true;
+//     }
+//     if (true) {
+//         return false;
+//     }
+// }
+// if (check()) {
+//     console.log('true')
+// } else {
+//     console.log('false');
+// }
+// if (false && true && true) {
+//     console.log(1);
+// }
+
+// let pass = '@wW12345';
+// let reg = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[\W_]).{6,16}/;
+// if (reg.test(pass)) {
+//     console.log('true');
+// } else {
+//     console.log('false');
+// }
+
+// function check1() {
+//     let pass = 'shen';
+// }
+// function check2() {
+//     // pass = 'hong';
+//     console.log(pass);
+// }
+// check2();
+
+// let a = 2;
+// var a = 2;
+function check() {
+    // var a = 3;
+    let a = 3;
+    console.log(a);
+}
+console.log(a);
+check();
