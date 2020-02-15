b30f7a97b8071e442fca89e38b5ce9ed72f80d4f
shenlink <2024133718@qq.com>
2020-02-15 23:12:12 +0800

    初步完成分页逻辑

--- app/model/User.php
+++ app/model/User.php
@@ -21,6 +21,7 @@ class User extends Model
     public function checkUsername($username)
     {
         // $_POST变量是超全局变量，可以直接在模型类获取表单提交的数据，但是，不提倡这么做，因为本来表单是提交到控制器的，表单传过来的值应该由控制器传到模型类中
+        // 这个$this是Model类的对象
         return $this->table('user')->where(array('username' => "{$username}"))->select();
     }
 
--- core/lib/Db.php
+++ core/lib/Db.php
@@ -15,8 +15,8 @@ class Db
     private $table;
     private $field = '*';
     private $order = '';
-    private $where='';
-    private $pdo;
+    private $where = '';
+    private $pdo = null;
     /**
      * @access:public
      * @name:__construct
@@ -36,7 +36,7 @@ class Db
         if (self::$db) {
             return self::$db;
         } else {
-            self::$db = new self;
+            self::$db = new self();
             return self::$db;
         }
     }
@@ -130,6 +130,67 @@ class Db
         $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $res;
     }
+    // 查询数据总数
+    public function count()
+    {
+        $sql = $this->fixSql('count');
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        $total = $stmt->fetchColumn(0);
+        return $total;
+    }
+
+    // 分页
+    // $arr = $table('user)->field('username')->where()->pagination();
+    public function pagination($currentPage, $pageSize = 10, $path = '/')
+    {
+        $count = $this->count();
+        $this->limit = ($currentPage - 1) * $pageSize . ',' . $pageSize;
+        $data = $this->selectAll();
+        $pageHtml = $this->seperatePage($currentPage, $pageSize, $count, $path);
+        return array('data' => $data, 'pageHtml' => $pageHtml);
+    }
+
+    // 生成分页html(bootstrap风格)；currentPage：当前第几页，pageSize:每页大小，total:数据总数
+    private function seperatePage($currentPage, $pageSize, $total, $path)
+    {
+        // 分页数，向上取整
+        $html = '';
+        // 56/10
+        $pageCount = ceil($total / $pageSize);
+        // 生成首页,生成上一页
+        if ($currentPage > 1) {
+            $html .= "<li class='page-item'><a class='page-link' href='{$path}/page/1'>首页</a></li>";
+
+            $prePage = $currentPage - 1;
+            $html .= "<li class='page-item'><a class='page-link' href='{$path}/page/{$prePage}'>上一页</a></li>";
+        }
+
+        // 生成数字页
+        // 假设分10页，最多显示7页
+        $start = $currentPage > ($pageCount - 6) ? ($pageCount - 6) : $currentPage;
+        $start = $start - 2;
+        $start = $start <= 0 ? 1 : $start;
+        $end = ($currentPage + 6) > $pageCount ? $pageCount : ($currentPage + 6);
+        $end = $end - 2;
+        if ($currentPage + 2 >= $end && $pageCount > 6) {
+            $start = $start + 2;
+            $end = $end + 2;
+        }
+
+        for ($i = $start; $i <= $end; $i++) {
+            $html .= $i == $currentPage ? "<li class='page-item active'><a class='page-link'>{$i}</a></li>" : "<li><a class='page-link' href='{$path}/page/{$i}'>{$i}</a></li>";
+        }
+        // 生成下一页,生成尾页
+        if ($currentPage < $pageCount) {
+            $nextPage = $currentPage + 1;
+            $html .= "<li class='page-item><a class='page-link' href='{$path}/page/{$nextPage}'>下一页</a></li>";
+            $html .= "<li class='page-item><a class='page-link' href='{$path}/page/{$pageCount}'>尾页</a></li>";
+        }
+
+        $html = '<ul class="pagination justify-content-center">' . $html . '</ul>';
+        return $html;
+    }
     /**
      * @access:public
      * @name:
@@ -171,16 +232,6 @@ class Db
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute();
         return $stmt->rowCount();
-    }
-    /**
-     * @access:public
-     * @name:field
-     * @param
-     * @return:
-     * @msg:
-     */
-    public function count()
-    {
     }
     /**
      * @access:public
@@ -226,6 +277,14 @@ class Db
                 $sql .= " limit {$this->limit}";
             }
         }
+
+        if ($type == 'count') {
+            $where = $this->fixWhere();
+            $fieldList = explode(',', $this->field);
+            $field = count($fieldList) > 1 ? '*' : $this->field;
+            $sql = "select count({$field}) from {$this->table} {$where}";
+        }
+
         if ($type === 'insert') {
             $sql = "insert into {$this->table}";
             $fields = $values = [];
