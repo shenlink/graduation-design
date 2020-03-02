d2092090b0c23cfbc250a37815c02e046e9ef31a
shenlink <2024133718@qq.com>
2020-03-02 12:28:22 +0800

    删除了model下的Validate类，修改了controller下的Validate类

--- app/controller/Validate.php
+++ app/controller/Validate.php
@@ -26,17 +26,10 @@ class Validate extends Controller
         session_start();
         if (isset($_SESSION['username'])) {
             $username = $_SESSION['username'];
-            $validate = new \app\model\Validate();
-            $urls = $validate->checkValidate($username);
-            $urls = explode(',', $urls);
-            foreach ($urls as $value) {
-                if ($value == '/user/write') {
-                    self::$access = '2';
-                }
-                if ($value == '/admin/manage') {
-                    self::$access = '1';
-                    break;
-                }
+            if ($username == 'shen') {
+                self::$access = '1';
+            } else {
+                self::$access = '2';
             }
         } else {
             self::$access = '3';
