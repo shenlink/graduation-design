e46884f816b3c8c7bfedbe07590b498da6bb960f
shenlink <2024133718@qq.com>
2020-03-07 14:17:12 +0800

    修复了数据库的问题

--- shen.sql
+++ shen.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : shen
+ Source Server         : shen3
  Source Server Type    : MySQL
  Source Server Version : 80012
  Source Host           : localhost:3306
- Source Schema         : shen
+ Source Schema         : shen3
 
  Target Server Type    : MySQL
  Target Server Version : 80012
  File Encoding         : 65001
 
- Date: 12/02/2020 18:18:43
+ Date: 07/03/2020 14:14:04
 */
 
 SET NAMES utf8mb4;
@@ -22,14 +22,11 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `announcement`;
 CREATE TABLE `announcement`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '公告id',
-  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '公告内容',
-  `created_at` datetime(0) NULL DEFAULT NULL COMMENT '公告创建时间',
-  `updated_at` datetime(0) NULL DEFAULT NULL COMMENT '公告更新时间',
-  `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '默认为1，显示公告，为0时不显示',
-  `soft` tinyint(1) NOT NULL DEFAULT 0 COMMENT '软删除，默认为1，直接删除，为0时删除',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `announcement_id` int(11) NOT NULL,
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `created_at` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`announcement_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for article
@@ -37,22 +34,24 @@ CREATE TABLE `announcement`  (
 DROP TABLE IF EXISTS `article`;
 CREATE TABLE `article`  (
   `article_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '文章id',
-  `author_id` int(11) NOT NULL COMMENT '作者id',
+  `author` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
   `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '文章标题',
   `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '文章内容',
-  `status` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '1' COMMENT '状态，1为正常，0为违规',
-  `hot` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否是热文，默认为0，不是，为1是热文',
+  `category` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '分类名称',
+  `status` tinyint(1) NULL DEFAULT 1,
+  `recommend` tinyint(1) NOT NULL DEFAULT 0,
   `created_at` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   `updated_at` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
-  `category_id` tinyint(1) NULL DEFAULT NULL COMMENT '分类id',
-  `comment_count` int(11) NULL DEFAULT NULL COMMENT '评论数量',
-  `praise_count` int(11) NULL DEFAULT NULL COMMENT '点赞数量',
-  `collect_count` int(11) NULL DEFAULT NULL COMMENT '收藏的数量',
+  `comment_count` int(11) NULL DEFAULT 0 COMMENT '评论数量',
+  `praise_count` int(11) NULL DEFAULT 0 COMMENT '点赞数量',
+  `collect_count` int(11) NULL DEFAULT 0 COMMENT '收藏的数量',
+  `share_count` int(11) NULL DEFAULT 0 COMMENT '分享的数量',
   PRIMARY KEY (`article_id`) USING BTREE,
-  UNIQUE INDEX `author_id`(`author_id`) USING BTREE,
-  INDEX `fk_category`(`category_id`) USING BTREE,
-  CONSTRAINT `fk_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT `fk_author` FOREIGN KEY (`author_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  INDEX `author`(`author`) USING BTREE,
+  INDEX `title`(`title`) USING BTREE,
+  INDEX `category`(`category`) USING BTREE,
+  CONSTRAINT `article_ibfk_1` FOREIGN KEY (`author`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `article_ibfk_2` FOREIGN KEY (`category`) REFERENCES `category` (`category`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -60,27 +59,33 @@ CREATE TABLE `article`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category`  (
-  `category_id` tinyint(1) NOT NULL COMMENT '分类id',
-  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '分类名称',
+  `category_id` tinyint(1) NOT NULL AUTO_INCREMENT COMMENT '分类id',
+  `category` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '分类名称',
   `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '状态',
-  `articles` int(11) NOT NULL DEFAULT 0 COMMENT '文章数',
-  PRIMARY KEY (`category_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `article_count` int(11) NULL DEFAULT 0 COMMENT '文章数',
+  PRIMARY KEY (`category_id`) USING BTREE,
+  INDEX `category`(`category`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for collect
 -- ----------------------------
 DROP TABLE IF EXISTS `collect`;
 CREATE TABLE `collect`  (
-  `user_id` int(11) NOT NULL COMMENT '用户id',
+  `collect_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户id',
   `article_id` int(11) NOT NULL COMMENT '文章id',
-  `created_at` datetime(0) NULL DEFAULT NULL COMMENT '收藏时间',
-  PRIMARY KEY (`user_id`) USING BTREE,
-  UNIQUE INDEX `user_id`(`user_id`) USING BTREE,
-  INDEX `fk_article`(`article_id`) USING BTREE,
-  CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_article` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `author` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `title` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `collect_at` datetime(0) NULL DEFAULT NULL COMMENT '收藏时间',
+  PRIMARY KEY (`collect_id`) USING BTREE,
+  INDEX `username`(`username`) USING BTREE,
+  INDEX `article_id`(`article_id`) USING BTREE,
+  INDEX `author`(`author`) USING BTREE,
+  CONSTRAINT `collect_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `collect_ibfk_2` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `collect_ibfk_3` FOREIGN KEY (`author`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for comment
@@ -89,71 +94,104 @@ DROP TABLE IF EXISTS `comment`;
 CREATE TABLE `comment`  (
   `comment_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '评论id',
   `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '评论内容',
-  `created_at` datetime(0) NOT NULL COMMENT '评论时间',
+  `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `article_id` int(11) NOT NULL COMMENT '文章id',
-  `user_id` int(11) NOT NULL COMMENT '用户id',
+  `title` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `author` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `comment_at` datetime(0) NULL DEFAULT NULL COMMENT '评论时间',
+  `status` tinyint(1) NOT NULL DEFAULT 1,
   PRIMARY KEY (`comment_id`) USING BTREE,
-  UNIQUE INDEX `article_id`(`article_id`) USING BTREE,
-  UNIQUE INDEX `user_id`(`user_id`) USING BTREE,
-  CONSTRAINT `fk_article3` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_user3` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  INDEX `username`(`username`) USING BTREE,
+  INDEX `article_id`(`article_id`) USING BTREE,
+  INDEX `author`(`author`) USING BTREE,
+  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`author`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `comment_ibfk_3` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for fans
+-- Table structure for follow
 -- ----------------------------
-DROP TABLE IF EXISTS `fans`;
-CREATE TABLE `fans`  (
-  `user_id` int(11) NOT NULL,
-  `fans_id` int(11) NOT NULL,
-  `addtime` datetime(0) NULL DEFAULT NULL,
-  PRIMARY KEY (`user_id`, `fans_id`) USING BTREE,
-  INDEX `fk_fans`(`fans_id`) USING BTREE,
-  CONSTRAINT `fk_user4` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_fans` FOREIGN KEY (`fans_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+DROP TABLE IF EXISTS `follow`;
+CREATE TABLE `follow`  (
+  `follow_id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `follow_at` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`follow_id`) USING BTREE,
+  INDEX `author`(`author`) USING BTREE,
+  INDEX `username`(`username`) USING BTREE,
+  CONSTRAINT `follow_ibfk_1` FOREIGN KEY (`author`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `follow_ibfk_2` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for follows
+-- Table structure for message
 -- ----------------------------
-DROP TABLE IF EXISTS `follows`;
-CREATE TABLE `follows`  (
-  `user_id` int(11) NOT NULL,
-  `follows_id` int(11) NOT NULL,
-  `addtime` datetime(0) NULL DEFAULT NULL,
-  PRIMARY KEY (`user_id`, `follows_id`) USING BTREE,
-  INDEX `fk_follows`(`follows_id`) USING BTREE,
-  CONSTRAINT `fk_follows` FOREIGN KEY (`follows_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_user5` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message`  (
+  `message_id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `created_at` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`message_id`) USING BTREE,
+  INDEX `author`(`author`) USING BTREE,
+  CONSTRAINT `message_ibfk_1` FOREIGN KEY (`author`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for praise
 -- ----------------------------
 DROP TABLE IF EXISTS `praise`;
 CREATE TABLE `praise`  (
-  `user_id` int(10) NOT NULL,
+  `praise_id` int(11) NOT NULL AUTO_INCREMENT,
   `article_id` int(10) NOT NULL,
-  `created_at` datetime(0) NOT NULL,
-  PRIMARY KEY (`user_id`) USING BTREE,
-  INDEX `fk_user6`(`article_id`) USING BTREE,
-  CONSTRAINT `fk_prase` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `fk_user6` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `author` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `title` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `praise_at` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`praise_id`) USING BTREE,
+  INDEX `username`(`username`) USING BTREE,
+  INDEX `article_id`(`article_id`) USING BTREE,
+  INDEX `author`(`author`) USING BTREE,
+  CONSTRAINT `praise_ibfk_1` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `praise_ibfk_2` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `praise_ibfk_3` FOREIGN KEY (`author`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for receive
+-- ----------------------------
+DROP TABLE IF EXISTS `receive`;
+CREATE TABLE `receive`  (
+  `receive_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `receive_at` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`receive_id`) USING BTREE,
+  INDEX `username`(`username`) USING BTREE,
+  CONSTRAINT `receive_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for share
 -- ----------------------------
 DROP TABLE IF EXISTS `share`;
 CREATE TABLE `share`  (
-  `user_id` int(11) NOT NULL COMMENT '用户id',
+  `share_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户id',
   `article_id` int(11) NOT NULL COMMENT '文章id',
-  `share_time` datetime(0) NULL DEFAULT NULL COMMENT '分享的时间',
-  PRIMARY KEY (`user_id`) USING BTREE,
-  UNIQUE INDEX `article_id`(`article_id`) USING BTREE,
-  CONSTRAINT `fk_user2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_article2` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `author` varchar(59) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `title` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `share_at` datetime(0) NULL DEFAULT NULL COMMENT '分享的时间',
+  PRIMARY KEY (`share_id`) USING BTREE,
+  INDEX `username`(`username`) USING BTREE,
+  INDEX `article_id`(`article_id`) USING BTREE,
+  INDEX `author`(`author`) USING BTREE,
+  CONSTRAINT `share_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `share_ibfk_2` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `share_ibfk_3` FOREIGN KEY (`author`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for user
@@ -162,20 +200,16 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `password` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `role` tinyint(1) NOT NULL DEFAULT 1,
-  `created_at` datetime(0) NOT NULL,
-  `information` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `article_count` int(11) NULL DEFAULT NULL,
-  `folloes_count` int(11) NULL DEFAULT NULL,
-  `fans_count` int(11) NULL DEFAULT NULL,
-  `soft` tinyint(1) NOT NULL DEFAULT 0 COMMENT '1为软删除，默认为0，直接删除',
-  PRIMARY KEY (`user_id`) USING BTREE
+  `password` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `role` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` datetime(0) NULL DEFAULT NULL,
+  `introduction` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `article_count` int(11) NULL DEFAULT 0,
+  `follow_count` int(11) NULL DEFAULT 0,
+  `fans_count` int(11) NULL DEFAULT 0,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`user_id`) USING BTREE,
+  INDEX `username`(`username`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES (1, 'shen', 'shen', 1, '2020-02-12 10:15:11', '管理员', 5, 5, 5, 0);
 
 SET FOREIGN_KEY_CHECKS = 1;
