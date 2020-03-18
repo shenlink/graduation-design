756c7128fc9982ab2f7690fcdb6add1d51243c70
shenlink <2024133718@qq.com>
2020-03-18 14:04:02 +0800

    解决了article页面点击关注没反应的问题

--- public/static/js/article.js
+++ public/static/js/article.js
@@ -237,6 +237,50 @@ function delComment(commentId) {
     }
 }
 
+$('#follow').on('click', function () {
+    let follow = document.querySelector('#follow');
+    let username = follow.getAttribute('data-username');
+    let author = follow.getAttribute('data-author');
+    if (username == '') {
+        layer.msg('登录才能关注', {
+            time: 2000
+        });
+        return;
+    }
+    // 1.创建XMLHttpRequest对象
+    let request = null;
+    if (XMLHttpRequest) {
+        request = new XMLHttpRequest();
+    } else {
+        //兼容老IE浏览器
+        request = new ActiveXObject("Msxml2.XMLHTTP");
+    }
+    // 2.请求行
+    request.open("POST", "/follow/checkFollow");
+    // 3.请求头
+    request.setRequestHeader('Content-Type', ' application/x-www-form-urlencoded');
+    // 4.设置数据
+    request.send("author=" + author + "&username=" + username);
+    // 5.监听服务器响应
+    request.onreadystatechange = function () {
+        if (request.readyState == 4 && request.status == 200) {
+            if (request.responseText == "1") {
+                layer.msg('关注成功', {
+                    time: 1000
+                }, function () {
+                    window.location.reload();
+                });
+            } else {
+                layer.msg('取消关注成功', {
+                    time: 1000
+                }, function () {
+                    window.location.reload();
+                });
+            }
+        }
+    }
+});
+
 function addMessage() {
     let message = document.querySelector('#message');
     let author = message.getAttribute('data-author');
