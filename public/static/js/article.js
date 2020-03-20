2dfa96bae8d06294868d136d0853b042750c6ddb
shenlink <2024133718@qq.com>
2020-03-20 21:40:33 +0800

    优化了article.js的ajax和其它

--- public/static/js/article.js
+++ public/static/js/article.js
@@ -6,13 +6,13 @@ $('#search').on('click', function () {
     document.body.appendChild(form);
     switch (type) {
         case '1':
-            input1 = createSearchInput('type', 'user');
+            input1 = createInput('type', 'user');
             break;
         case '2':
-            input1 = createSearchInput('type', 'article');
+            input1 = createInput('type', 'article');
             break;
     }
-    let input2 = createSearchInput('content', searchContent);
+    let input2 = createInput('content', searchContent);
     form.appendChild(input1);
     form.appendChild(input2);
     form.method = 'post';
@@ -24,7 +24,8 @@ $('#search').on('click', function () {
     form.submit();
 });
 
-function createSearchInput(name, value) {
+
+function createInput(name, value) {
     let input = document.createElement('input');
     input.type = 'hidden';
     input.name = name;
@@ -48,36 +49,27 @@ $('#praise').on('click', function () {
         });
         return;
     }
-    // 1.创建XMLHttpRequest对象
-    let request = null;
-    if (XMLHttpRequest) {
-        request = new XMLHttpRequest();
-    } else {
-        //兼容老IE浏览器
-        request = new ActiveXObject("Msxml2.XMLHTTP");
-    }
-    // 2.请求行
-    request.open("POST", "/praise/checkPraise");
-    // 3.请求头
-    request.setRequestHeader('Content-Type', ' application/x-www-form-urlencoded');
-    // 4.设置数据
-    request.send("username=" + username + "&article_id=" + article_id + "&author=" + author + "&title=" + title);
-    // 5.监听服务器响应
-    request.onreadystatechange = function () {
-        if (request.readyState == 4 && request.status == 200) {
-            if (request.responseText == "1") {
-                layer.msg('点赞成功', {
-                    time: 1000
-                });
+    $.post("/praise/checkPraise", {
+        username: username,
+        article_id: article_id,
+        author: author,
+        title: title
+    }, function (data) {
+        if (data === '1') {
+            layer.msg('点赞成功', {
+                time: 1000
+            }, function () {
                 praise.innerHTML = `已点赞(${praise_count+1})&nbsp;&nbsp;&nbsp;&nbsp;`;
-            } else {
-                layer.msg('取消点赞', {
-                    time: 1000
-                });
-                praise.innerHTML = `点赞(${praise_count-1})&nbsp;&nbsp;&nbsp;&nbsp;`;
-            }
+            });
         }
-    }
+        if (data === '0') {
+            layer.msg('取消点赞', {
+                time: 1000
+            }, function () {
+                praise.innerHTML = `点赞(${praise_count-1})&nbsp;&nbsp;&nbsp;&nbsp;`;
+            });
+        }
+    });
 });
 
 
@@ -96,36 +88,27 @@ $('#collect').on('click', function () {
         });
         return;
     }
-    // 1.创建XMLHttpRequest对象
-    let request = null;
-    if (XMLHttpRequest) {
-        request = new XMLHttpRequest();
-    } else {
-        //兼容老IE浏览器
-        request = new ActiveXObject("Msxml2.XMLHTTP");
-    }
-    // 2.请求行
-    request.open("POST", "/collect/checkCollect");
-    // 3.请求头
-    request.setRequestHeader('Content-Type', ' application/x-www-form-urlencoded');
-    // 4.设置数据
-    request.send("username=" + username + "&article_id=" + article_id + "&author=" + author + "&title=" + title);
-    // 5.监听服务器响应
-    request.onreadystatechange = function () {
-        if (request.readyState == 4 && request.status == 200) {
-            if (request.responseText == "1") {
-                layer.msg('收藏成功', {
-                    time: 1000
-                });
+    $.post("/collect/checkCollect", {
+        username: username,
+        article_id: article_id,
+        author: author,
+        title: title
+    }, function (data) {
+        if (data === '1') {
+            layer.msg('收藏成功', {
+                time: 1000
+            }, function () {
                 collect.innerHTML = `已收藏(${collect_count+1})&nbsp;&nbsp;&nbsp;&nbsp;`;
-            } else {
-                layer.msg('取消收藏', {
-                    time: 1000
-                });
-                collect.innerHTML = `收藏(${collect_count-1})&nbsp;&nbsp;&nbsp;&nbsp;`;
-            }
+            });
         }
-    }
+        if (data === '0') {
+            layer.msg('取消收藏', {
+                time: 1000
+            }, function () {
+                collect.innerHTML = `收藏(${collect_count-1})&nbsp;&nbsp;&nbsp;&nbsp;`;
+            });
+        }
+    });
 });
 
 // 分享
@@ -143,36 +126,27 @@ $('#share').on('click', function () {
         });
         return;
     }
-    // 1.创建XMLHttpRequest对象
-    let request = null;
-    if (XMLHttpRequest) {
-        request = new XMLHttpRequest();
-    } else {
-        //兼容老IE浏览器
-        request = new ActiveXObject("Msxml2.XMLHTTP");
-    }
-    // 2.请求行
-    request.open("POST", "/share/checkShare");
-    // 3.请求头
-    request.setRequestHeader('Content-Type', ' application/x-www-form-urlencoded');
-    // 4.设置数据
-    request.send("username=" + username + "&article_id=" + article_id + "&title=" + title + "&author=" + author);
-    // 5.监听服务器响应
-    request.onreadystatechange = function () {
-        if (request.readyState == 4 && request.status == 200) {
-            if (request.responseText == "1") {
-                layer.msg('分享成功', {
-                    time: 1000
-                });
+    $.post("/share/checkShare", {
+        username: username,
+        article_id: article_id,
+        author: author,
+        title: title
+    }, function (data) {
+        if (data === '1') {
+            layer.msg('分享成功', {
+                time: 1000
+            }, function () {
                 share.innerHTML = `已分享(${share_count+1})&nbsp;&nbsp;&nbsp;&nbsp;`;
-            } else {
-                layer.msg('取消分享', {
-                    time: 1000
-                });
-                share.innerHTML = `分享(${share_count-1})&nbsp;&nbsp;&nbsp;&nbsp;`;
-            }
+            });
         }
-    }
+        if (data === '0') {
+            layer.msg('分享失败', {
+                time: 1000
+            }, function () {
+                share.innerHTML = `分享(${share_count-1})&nbsp;&nbsp;&nbsp;&nbsp;`;
+            });
+        }
+    });
 });
 
 function createTime() {
@@ -205,35 +179,36 @@ function createTime() {
 
 function createComment(username, comment_at, comment_id, content) {
     let comment_html = `<div class="card-body">
-                                    <h5 class="card-title">
-                                        <span>${username}</span>
-                                        <span>
-                                            <small class="offset-md-1">
-                                                ${comment_at}
-                                            </small>
-                                        </span>
-                                        <div>
-                                            <small class="delComment"
-                                                onclick="delComment(this)"
-                                                data-comment-id=
-                                                ${comment_id}>删除
-                                            </small>
-                                        </div>
-                                    </h5>
-                                    <div class="card-text">
-                                        ${content}
-                                    </div>
-                                </div>`;
+                            <h5 class="card-title">
+                                <span>${username}</span>
+                                <span>
+                                    <small class="offset-md-1">
+                                        ${comment_at}
+                                    </small>
+                                </span>
+                                <div>
+                                    <small class="delComment"
+                                        onclick="delComment(this)"
+                                        data-comment-id=
+                                        ${comment_id}>删除
+                                    </small>
+                                </div>
+                            </h5>
+                            <div class="card-text">
+                                ${content}
+                            </div>
+                        </div>`;
     let div = document.createElement("div");
     div.setAttribute('class', 'card');
     div.innerHTML = comment_html;
     return div;
 }
 
+
+// 登录后才能评论，评论内容去掉标签，空格后为空的话，不能评论
 let E = window.wangEditor;
 let editor = new E('#editor-toolbar', '#editor-content');
 editor.create();
-// 登录后才能评论，评论内容去掉标签，空格后为空的话，不能评论
 $('#comment').on('click', function () {
     let html = editor.txt.html();
     let content = filterXSS(html);
@@ -248,7 +223,6 @@ $('#comment').on('click', function () {
     let count = document.querySelector('#comment-count');
     let comment_count = parseInt(count.innerText.replace(/[^0-9]/ig, ""));
     let comment_at = createTime();
-    // 1.创建XMLHttpRequest对象
     if (username == '') {
         layer.msg('登录才能评论', {
             time: 2000
@@ -261,39 +235,30 @@ $('#comment').on('click', function () {
         });
         return;
     }
-    let request = null;
-    if (XMLHttpRequest) {
-        request = new XMLHttpRequest();
-    } else {
-        //兼容老IE浏览器
-        request = new ActiveXObject("Msxml2.XMLHTTP");
-    }
-    // 2.请求行
-    request.open("POST", "/comment/addComment");
-    // 3.请求头
-    request.setRequestHeader('Content-Type', ' application/x-www-form-urlencoded');
-    // 4.设置数据
-    request.send("article_id=" + article_id + "&author=" + author + "&title=" + title + "&content=" + content + "&username=" + username);
-    // 5.监听服务器响应
-    request.onreadystatechange = function () {
-        if (request.readyState == 4 && request.status == 200) {
-            if (request.responseText != "0") {
-                layer.msg('评论成功', {
-                    time: 1000
-                }, function () {
-                    comment_id = request.responseText;
-                    let div = createComment(username, comment_at, comment_id, content);
-                    commentContent.insertBefore(div, commentContent.children[0]);
-                    count.innerHTML = `评论数：${comment_count + 1}`;
-                    editor.txt.html('');
-                });
-            } else {
-                layer.msg('评论失败', {
-                    time: 1000
-                });
-            }
+    $.post("/comment/addComment", {
+        username: username,
+        article_id: article_id,
+        author: author,
+        title: title,
+        content: content
+    }, function (data) {
+        if (data !== '0') {
+            layer.msg('评论成功', {
+                time: 1000
+            }, function () {
+                comment_id = data;
+                let div = createComment(username, comment_at, comment_id, content);
+                commentContent.insertBefore(div, commentContent.children[0]);
+                count.innerHTML = `评论数：${comment_count + 1}`;
+                editor.txt.html('');
+            });
         }
-    }
+        if (data === '0') {
+            layer.msg('评论失败', {
+                time: 1000
+            });
+        }
+    });
 });
 
 // 删除评论
@@ -308,40 +273,29 @@ function delComment(commentId) {
     let count = document.querySelector('#comment-count');
     let comment_count = parseInt(count.innerText.replace(/[^0-9]/ig, ""));
     let commentContent = document.querySelector('#comment-content');
-    // 1.创建XMLHttpRequest对象
-    let request = null;
-    if (XMLHttpRequest) {
-        request = new XMLHttpRequest();
-    } else {
-        //兼容老IE浏览器
-        request = new ActiveXObject("Msxml2.XMLHTTP");
-    }
-    // 2.请求行
-    request.open("POST", "/comment/delComment");
-    // 3.请求头
-    request.setRequestHeader('Content-Type', ' application/x-www-form-urlencoded');
-    // 4.设置数据
-    request.send("comment_id=" + comment_id + "&article_id=" + article_id);
-    // 5.监听服务器响应
-    request.onreadystatechange = function () {
-        if (request.readyState == 4 && request.status == 200) {
-            if (request.responseText == "1") {
-                layer.msg('删除成功', {
-                    time: 1000
-                }, function () {
-                    let card = temp.parentNode.parentNode.parentNode.parentNode;
-                    commentContent.removeChild(card);
-                    count.innerHTML = `评论数：${comment_count-1}`;
-                });
-            } else {
-                layer.msg('删除失败', {
-                    time: 1000
-                });
-            }
+    $.post("/comment/delComment", {
+        article_id: article_id,
+        comment_id: comment_id
+    }, function (data) {
+        if (data === '1') {
+            layer.msg('删除成功', {
+                time: 1000
+            }, function () {
+                let card = temp.parentNode.parentNode.parentNode.parentNode;
+                commentContent.removeChild(card);
+                count.innerHTML = `评论数：${comment_count-1}`;
+            });
         }
-    }
+        if (data === '0') {
+            layer.msg('删除失败', {
+                time: 1000
+            });
+        }
+    });
 }
 
+
+// 关注或取消关注
 $('#follow').on('click', function () {
     let follow = document.querySelector('#follow');
     let username = follow.getAttribute('data-username');
@@ -352,38 +306,25 @@ $('#follow').on('click', function () {
         });
         return;
     }
-    // 1.创建XMLHttpRequest对象
-    let request = null;
-    if (XMLHttpRequest) {
-        request = new XMLHttpRequest();
-    } else {
-        //兼容老IE浏览器
-        request = new ActiveXObject("Msxml2.XMLHTTP");
-    }
-    // 2.请求行
-    request.open("POST", "/follow/checkFollow");
-    // 3.请求头
-    request.setRequestHeader('Content-Type', ' application/x-www-form-urlencoded');
-    // 4.设置数据
-    request.send("author=" + author + "&username=" + username);
-    // 5.监听服务器响应
-    request.onreadystatechange = function () {
-        if (request.readyState == 4 && request.status == 200) {
-            if (request.responseText == "1") {
-                layer.msg('关注成功', {
-                    time: 1000
-                }, function () {
-                    window.location.reload();
-                });
-            } else {
-                layer.msg('取消关注成功', {
-                    time: 1000
-                }, function () {
-                    window.location.reload();
-                });
-            }
+    $.post("/follow/checkFollow", {
+        author: author,
+        username: username
+    }, function (data) {
+        if (data === '1') {
+            layer.msg('关注成功', {
+                time: 1000
+            }, function () {
+                follow.innerText = '已关注';
+            });
         }
-    }
+        if (data === '0') {
+            layer.msg('取消关注', {
+                time: 1000
+            }, function () {
+                follow.innerText = '关注';
+            });
+        }
+    });
 });
 
 function addMessage() {
@@ -391,17 +332,9 @@ function addMessage() {
     let author = message.getAttribute('data-author');
     let form = document.createElement("form");
     document.body.appendChild(form);
-    let input = createMessageInput('author', author);
+    let input = createInput('author', author);
     form.appendChild(input);
     form.method = 'post';
     form.action = '/message/addMessage';
     form.submit();
-}
-
-function createMessageInput(name, value) {
-    let input = document.createElement('input');
-    input.type = 'hidden';
-    input.name = name;
-    input.value = value;
-    return input;
 }
