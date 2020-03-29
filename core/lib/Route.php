1876f0cb058b673a2500b9140f29245d226f85a1
shenlink <2024133718@qq.com>
2020-03-29 14:02:15 +0800

    把Route的pathArray的3和4的规则放到一个函数中

--- core/lib/Route.php
+++ core/lib/Route.php
@@ -28,120 +28,126 @@ class Route extends Db
             if (isset($pathArray[1])) {
                 $this->action = $pathArray[1];
             }
-            if (isset($pathArray[2]) && !isset($pathArray[3])) {
-                $this->displayNone();
-            }
-            if (isset($pathArray[3])) {
-                $controllerArray = ['admin', 'category', 'index'];
-                if (in_array($pathArray[0], $controllerArray)) {
-                    if (!preg_match('/^([1-9][0-9]*){1,10}$/', $pathArray[3])) {
-                        $this->displayNone();
-                    }
-                }
-            }
-            if ($pathArray[0] == 'admin' && $pathArray[1] == 'manage') {
-                $typeArray = ['user', 'article', 'category', 'comment', 'announcement', 'message'];
-                if (in_array($pathArray[2], $typeArray)) {
-                    if (!preg_match('/^([1-9][0-9]*){1,10}$/', $pathArray[3])) {
-                        $this->displayNone();
-                    }
-                    $this->type = $pathArray[2];
-                    $this->pagination = $pathArray[3];
-                }
-            }
+            $this->rule($pathArray);
+        } else {
+            $this->controller = Config::get('DEFAULT_CONTROLLER', 'route');
+            $this->action = Config::get('DEFAULT_ACTION', 'route');
+        }
+    }
 
-            if ($pathArray[0] == 'article' && $pathArray[1] == 'search') {
+    public function rule($pathArray)
+    {
+        if (isset($pathArray[2]) && !isset($pathArray[3])) {
+            $this->displayNone();
+        }
+        if (isset($pathArray[3])) {
+            $controllerArray = ['admin', 'category', 'index'];
+            if (in_array($pathArray[0], $controllerArray)) {
+                if (!preg_match('/^([1-9][0-9]*){1,10}$/', $pathArray[3])) {
+                    $this->displayNone();
+                }
+            }
+        }
+
+        if ($pathArray[0] == 'admin' && $pathArray[1] == 'manage') {
+            $typeArray = ['user', 'article', 'category', 'comment', 'announcement', 'message'];
+            if (in_array($pathArray[2], $typeArray)) {
                 if (!preg_match('/^([1-9][0-9]*){1,10}$/', $pathArray[3])) {
                     $this->displayNone();
                 }
                 $this->type = $pathArray[2];
                 $this->pagination = $pathArray[3];
             }
+        }
 
-            if ($pathArray[0] == 'article' && $pathArray[1] == 'editArticle') {
-                if (!preg_match('/^([1-9][0-9]*){1,10}$/', $pathArray[3])) {
-                    $this->displayNone();
-                }
-                if ($pathArray[2] == 'article') {
-                    $this->type = $pathArray[2];
-                    $this->pagination = $pathArray[3];
-                } else {
-                    $this->displayNone();
-                }
+        if ($pathArray[0] == 'article' && $pathArray[1] == 'search') {
+            if (!preg_match('/^([1-9][0-9]*){1,10}$/', $pathArray[3])) {
+                $this->displayNone();
             }
+            $this->type = $pathArray[2];
+            $this->pagination = $pathArray[3];
+        }
 
-            if ($pathArray[0] == 'category') {
-                $category = $this->table('category')->field('category')->where(['category' => "{$pathArray[1]}"])->select();
-                if ($category) {
-                    $this->type = 'pagination';
-                    $this->pagination = $pathArray[3];
-                }
-                if (isset($pathArray[2]) && $pathArray[2] != 'pagination') {
-                    $this->displayNone();
-                }
+        if ($pathArray[0] == 'article' && $pathArray[1] == 'editArticle') {
+            if (!preg_match('/^([1-9][0-9]*){1,10}$/', $pathArray[3])) {
+                $this->displayNone();
             }
+            if ($pathArray[2] == 'article') {
+                $this->type = $pathArray[2];
+                $this->pagination = $pathArray[3];
+            } else {
+                $this->displayNone();
+            }
+        }
 
-            if ($pathArray[0] == 'index' && $pathArray[1] == 'index' && $pathArray[2] == 'pagination') {
+        if ($pathArray[0] == 'category') {
+            $category = $this->table('category')->field('category')->where(['category' => "{$pathArray[1]}"])->select();
+            if ($category) {
                 $this->type = 'pagination';
                 $this->pagination = $pathArray[3];
             }
-            if ($pathArray[0] == 'index' && $pathArray[1] == 'index' && $pathArray[2] != 'pagination') {
+            if (isset($pathArray[2]) && $pathArray[2] != 'pagination') {
                 $this->displayNone();
             }
+        }
 
-            if ($pathArray[0] == 'user') {
-                $manageType = ['article', 'comment', 'follow', 'fans'];
-                $userType = ['article', 'comment', 'praise', 'collect', 'share'];
-                $username = $this->table('user')->where(['username' => "{$pathArray[1]}", 'status' => 1])->select();
-                if ($pathArray[1] == 'manage') {
-                    if (isset($pathArray[3]) && !preg_match('/^([1-9][0-9]*){1,10}$/', $pathArray[3])) {
-                        $this->displayNone();
-                    }
-                    if (in_array($pathArray[2], $manageType)) {
-                        $this->type = $pathArray[2];
-                        $this->pagination = $pathArray[3];
-                    }
-                    if (isset($pathArray[2]) && !in_array($pathArray[2], $manageType)) {
-                        $this->displayNone();
-                    }
-                }
-                if ($pathArray[1] == 'search') {
-                    if (mb_strlen($pathArray[2]) <= 16) {
-                        if (!preg_match('/^([1-9][0-9]*){1,10}$/', $pathArray[3])) {
-                            $this->displayNone();
-                        }
-                        $this->type = $pathArray[2];
-                        $this->pagination = $pathArray[3];
-                    } else {
-                        $this->displayNone();
-                    }
-                }
-                if ($username) {
-                    if (in_array($pathArray[2], $userType)) {
-                        $this->type = $pathArray[2];
-                        $this->pagination = $pathArray[3];
-                    }
-                    if (isset($pathArray[2]) && !in_array($pathArray[2], $userType)) {
-                        $this->displayNone();
-                    }
-                }
-            }
+        if ($pathArray[0] == 'index' && $pathArray[1] == 'index' && $pathArray[2] == 'pagination') {
+            $this->type = 'pagination';
+            $this->pagination = $pathArray[3];
+        }
+        if ($pathArray[0] == 'index' && $pathArray[1] == 'index' && $pathArray[2] != 'pagination') {
+            $this->displayNone();
+        }
 
-            if ($pathArray[0] == 'message' && $pathArray[1] == 'addMessage') {
-                $username = $this->table('user')->field('username')->where(['username' => "{$pathArray[3]}", 'status' => 1])->select();
-                if (!$username) {
+        if ($pathArray[0] == 'user') {
+            $manageType = ['article', 'comment', 'follow', 'fans'];
+            $userType = ['article', 'comment', 'praise', 'collect', 'share'];
+            $username = $this->table('user')->where(['username' => "{$pathArray[1]}", 'status' => 1])->select();
+            if ($pathArray[1] == 'manage') {
+                if (isset($pathArray[3]) && !preg_match('/^([1-9][0-9]*){1,10}$/', $pathArray[3])) {
                     $this->displayNone();
                 }
-                if ($pathArray[2] == 'username') {
+                if (in_array($pathArray[2], $manageType)) {
+                    $this->type = $pathArray[2];
+                    $this->pagination = $pathArray[3];
+                }
+                if (isset($pathArray[2]) && !in_array($pathArray[2], $manageType)) {
+                    $this->displayNone();
+                }
+            }
+            if ($pathArray[1] == 'search') {
+                if (mb_strlen($pathArray[2]) <= 16) {
+                    if (!preg_match('/^([1-9][0-9]*){1,10}$/', $pathArray[3])) {
+                        $this->displayNone();
+                    }
                     $this->type = $pathArray[2];
                     $this->pagination = $pathArray[3];
                 } else {
                     $this->displayNone();
                 }
             }
-        } else {
-            $this->controller = Config::get('DEFAULT_CONTROLLER', 'route');
-            $this->action = Config::get('DEFAULT_ACTION', 'route');
+            if ($username) {
+                if (in_array($pathArray[2], $userType)) {
+                    $this->type = $pathArray[2];
+                    $this->pagination = $pathArray[3];
+                }
+                if (isset($pathArray[2]) && !in_array($pathArray[2], $userType)) {
+                    $this->displayNone();
+                }
+            }
+        }
+
+        if ($pathArray[0] == 'message' && $pathArray[1] == 'addMessage') {
+            $username = $this->table('user')->field('username')->where(['username' => "{$pathArray[3]}", 'status' => 1])->select();
+            if (!$username) {
+                $this->displayNone();
+            }
+            if ($pathArray[2] == 'username') {
+                $this->type = $pathArray[2];
+                $this->pagination = $pathArray[3];
+            } else {
+                $this->displayNone();
+            }
         }
     }
 
